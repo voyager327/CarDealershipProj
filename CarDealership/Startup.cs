591532f109b0d53842ca518120ef5fb3eebd695c
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-05-12 10:12:57 -0500

    this is with signalR installed and working this was a difficult task but should be good to go and error free

--- CarDealership/Startup.cs
+++ CarDealership/Startup.cs
@@ -12,6 +12,7 @@ using CarDealership.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CarDealership.SignalRChat.Hubs;
 
 namespace CarDealership
 {
@@ -34,6 +35,7 @@ namespace CarDealership
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +66,7 @@ namespace CarDealership
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
