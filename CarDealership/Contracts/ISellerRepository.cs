141abdbe475fefe96cf59bc7759d119e624a61a9
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-05-13 09:05:01 -0500

    this is with some small logic like the messaging added to the JS file that i also renamed to index

--- CarDealership/Contracts/ISellerRepository.cs
+++ CarDealership/Contracts/ISellerRepository.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace CarDealership.Contracts
 {
-<<<<<<< HEAD
+
     public interface ISellerRepository : IRepositoryBase<Seller>
-=======
-    public interface ISellerRepository
->>>>>>> 21b40c480bbd1c9e95e50bad8da1b185c8215e5c
     {
     }
 }
