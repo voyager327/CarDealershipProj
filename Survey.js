4a62c1d2503fac1c05792fe021c8c974947b8151
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-05-14 17:02:30 -0500

    this is with the second question fiished i started working on the third and decided to go with a better plan and try to get some ideas on paper before i made stuff that i would have to take out cause it wasnt right

--- Survey.js
+++ Survey.js
@@ -33,24 +33,26 @@ function firstQuestion(answers){
 }
 
 function secondQuestion(answers){
-    let questionTwo = promptFor("What is your primary reason for looking to purchase this vehicle?" + "\n" + "Is it for Work?(i.e. towing, hauling, plowing, ect." + "\n" + "Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)" + "\n" + "Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)" + "\n" + " Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"+ "\n" + " Please enter 1, 2, 3, or 4", chars);
+    let questionTwo = promptFor("What is your primary reason for looking to purchase this vehicle?" + "\n" + "Is it for Work?(i.e. towing, hauling, plowing, ect." + "\n" + "Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)" + "\n" + "Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)" + "\n" + " Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"+ "\n" + "Is this to be better for the enviroment?(i.e. would you like an electric vehicle, something with lower emmissions)" + "\n" + " Please enter 1, 2, 3, 4, or 5", chars);
     let secondQuestionAnswer;
     if (vehicle.reason === questionTwo){
         switch(reason){
-            case '1':
+            case '1': //"Is it for Work?(i.e. towing, hauling, plowing, ect."
                 secondQuestionAnswer = thirdQuestion(answers);
             break;
-            case '2':
+            case '2': //"Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)"
                 secondQuestionAnswer = fourthQuestion(answers);
             break;
-            case '3':
+            case '3': //"Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)"
                 secondQuestionAnswer = fifthQuestion(answers);
             break;
-            case '4':
+            case '4': //" Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"
                 secondQuestionAnswer = sixthQuestion(answers);
             break;
+            case '5': //"is this to be better for the enviroment?(i.e. would you like an electric vehicle, something with lower emmissions)
+                secondQuestionAnswer = seventhQuestion(answers);
             default:
-                secondQuestion(answers); // reask the question 
+                secondQuestion(answers); // re ask the question 
             break;
         }
         return secondQuestionAnswer;
@@ -61,7 +63,8 @@ function secondQuestion(answers){
     }
 }
 
-
+function thirdQuestion(answers)
+let questionThree = promptFor("")
 
 
 
