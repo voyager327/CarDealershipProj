7e2a2624bd097240c23e430d320a58a385286f28
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-05-14 10:18:14 -0500

    this is my innital commit with my survey started i have a few prelimanary questions and this is going to be where the survey starts to deviate based on answers

---
+++ Survey.js
@@ -0,0 +1,96 @@
+"use strict"
+
+
+function survey(answers){
+    let surveyStarter = promptFor("Welcome to our Buying and Selling Webapp." + "\n" + "If you know what kind of car you are looking for then be our guest look around for what you like." + "\n" + "However if you are unsure of what you might like we encourage you to take our survey that will give you stlyes as well as vehicles that we think you might like based on your needs." + "\n" + "Would you like to take our survey? Please enter 'yes' or 'no'", yesNo).toLocaleLowerCase();
+    let surveyStarterAnswer;
+    let surveyStarterDenied; 
+    switch(surveyStarter){
+        case 'yes':
+            surveyStarterAnswer = firstQuestion(answers);
+        break;
+        case 'no':
+            surveyStarterDenied = breakout();
+        break;
+        default:
+            survey(answers); // restart the app
+        break;
+    }
+}
+
+function firstQuestion(answers){
+    let questionOne = promptFor("How many people do you typically drive around on a regular basis?" + "\n" + "is it '1' ,'2','3','4', or '5+'" + "\n" + " Please enter 1, 2, 3, 4, or 5", chars);
+    let firstQuestionAnswer;
+    if (vehicle.passangers === questionOne){
+        return secondQuestion(answers);
+    }
+    else{ 
+        return breakout();
+    }
+    displayVehicle(matchingVehicle);
+    return matchingVehicle;
+
+}
+
+function secondQuestion(answers){
+    let questionTwo = promptFor("What is your primary reason for looking to purchase this vehicle?" + "\n" + "Is it for Work?(i.e. towing, hauling, plowing, ect." + "\n" + "Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)" + "\n" + "Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)" + "\n" + " Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"+ "\n" + " Please enter 1, 2, 3, or 4", chars);
+    let secondQuestionAnswer;
+    if (vehicle.reason === questionTwo){
+        switch(reason){
+            case '1':
+                secondQuestionAnswer = thirdQuestion(answers);
+            break;
+            case '2':
+                secondQuestionAnswer = fourthQuestion(answers);
+            break;
+            case '3':
+                secondQuestionAnswer = fifthQuestion(answers);
+            break;
+            case '4':
+                secondQuestionAnswer = sixthQuestion(answers);
+            break;
+            default:
+                secondQuestion(answers); // reask the question 
+            break;
+        }
+        return secondQuestionAnswer;
+
+    }
+    else{
+        return breakout();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function that prompts and validates user input
+function promptFor(question, valid){
+    do{
+      var response = prompt(question).trim();
+    } while(!response || !valid(response));
+    return response;
+  }
+  
+  // helper function to pass into promptFor to validate yes/no answers
+  function yesNo(input){
+    return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
+  }
+  
+  // helper function to pass in as default promptFor validation
+  function chars(input){
+    return true; // default validation only
+  }
+  
