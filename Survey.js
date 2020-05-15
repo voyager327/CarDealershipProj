58f81b0295c97eabb8a03b468c9929907a7261c0
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-05-15 09:18:55 -0500

    this is reformatted i had to change the order of the questions because it wouldnt have been able to run correctly otherwise i also added in some logic on more questions

--- Survey.js
+++ Survey.js
@@ -19,40 +19,26 @@ function survey(answers){
 }
 
 function firstQuestion(answers){
-    let questionOne = promptFor("How many people do you typically drive around on a regular basis?" + "\n" + "is it '1' ,'2','3','4', or '5+'" + "\n" + " Please enter 1, 2, 3, 4, or 5", chars);
+    let questionOne = promptFor("What is your primary reason for looking to purchase this vehicle?" + "\n" + "Is it for Work?(i.e. towing, hauling, plowing, ect." + "\n" + "Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)" + "\n" + "Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)" + "\n" + " Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"+ "\n" + "Is this to be better for the enviroment?(i.e. would you like an electric vehicle, something with lower emmissions)" + "\n" + " Please enter 1, 2, 3, 4, or 5", chars);
     let firstQuestionAnswer;
-    if (vehicle.passangers === questionOne){
-        return secondQuestion(answers);
-    }
-    else{ 
-        return breakout();
-    }
-    displayVehicle(matchingVehicle);
-    return matchingVehicle;
-
-}
-
-function secondQuestion(answers){
-    let questionTwo = promptFor("What is your primary reason for looking to purchase this vehicle?" + "\n" + "Is it for Work?(i.e. towing, hauling, plowing, ect." + "\n" + "Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)" + "\n" + "Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)" + "\n" + " Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"+ "\n" + "Is this to be better for the enviroment?(i.e. would you like an electric vehicle, something with lower emmissions)" + "\n" + " Please enter 1, 2, 3, 4, or 5", chars);
-    let secondQuestionAnswer;
-    if (vehicle.reason === questionTwo){
+    if (vehicle.reason === questionOne){
         switch(reason){
             case '1': //"Is it for Work?(i.e. towing, hauling, plowing, ect."
-                secondQuestionAnswer = thirdQuestion(answers);
+                firstQuestionAnswer = secondQuestion(answers);
             break;
             case '2': //"Is it for Casual usage?(i.e. running errands, picking up groceries, ect.)"
-                secondQuestionAnswer = fourthQuestion(answers);
+                firstQuestionAnswer = thirdQuestion(answers);
             break;
             case '3': //"Is it for Business/Luxuary? (i.e. bussiness excutive, high quality, afforadable luxuary class, ect.)"
-                secondQuestionAnswer = fifthQuestion(answers);
+                firstQuestionAnswer = fourthQuestion(answers);
             break;
             case '4': //" Is this for Fun/Recreational (i.e. camping, kayaking, wall crawling, ect.)"
-                secondQuestionAnswer = sixthQuestion(answers);
+                firstQuestionAnswer = fifthQuestion(answers);
             break;
             case '5': //"is this to be better for the enviroment?(i.e. would you like an electric vehicle, something with lower emmissions)
-                secondQuestionAnswer = seventhQuestion(answers);
+                firstQuestionAnswer = sixthQuestion(answers);
             default:
-                secondQuestion(answers); // re ask the question 
+                firstQuestion(answers); // re ask the question 
             break;
         }
         return secondQuestionAnswer;
@@ -61,10 +47,29 @@ function secondQuestion(answers){
     else{
         return breakout();
     }
+
+}
+
+function secondQuestion
+}
+
+function secondQuestion(answers)
+let questionTwo = promptFor("Would you like more power, or more versatility?" + "\n" + "Please enter 1 for more power, or 2 for more versatility." , chars);
+let secondQuestionAnswer;
+switch(questionTwo){
+    case '1':
+        secondQuestionAnswer = seventhQuestion(answers)
+    break;
+    case '2':
+        secondQuestionAnswer = eighthQuestion(answers)
+    break;
+    default:
+        secondQuestion(answers); // re ask the question 
+    break;
 }
 
 function thirdQuestion(answers)
-let questionThree = promptFor("")
+
 
 
 
